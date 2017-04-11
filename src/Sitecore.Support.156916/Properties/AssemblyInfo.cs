2ef0cd9e9f0e9ecd1fdd4582dd2a666968317606
Viktor Kucherov <vkucherov@live.ru>
2017-04-11 13:41:16 +0800

    no message

---
+++ src/Sitecore.Support.156916/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.156916")]
+[assembly: AssemblyProduct("Sitecore.Support.156916")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.156916/Shell/Applications/WebEdit/Dialogs/LockedItems/LockedItemsPage.cs
@@ -0,0 +1,49 @@
+﻿using ComponentArt.Web.UI;
+using Sitecore.Controls;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Extensions;
+using Sitecore.Globalization;
+using Sitecore.Web.UI.Grids;
+using Sitecore.Web.UI.Sheer;
+using Sitecore.Web.UI.XamlSharp.Ajax;
+using Sitecore.Web.UI.XamlSharp.Xaml;
+using System;
+
+namespace Sitecore.Support.Shell.Applications.WebEdit.Dialogs.LockedItems
+{
+  public class LockedItemsPage : ModalDialogPage
+  {
+    // Fields
+    protected Grid Items;
+
+    // Methods
+    protected override void ExecuteAjaxCommand(AjaxCommandEventArgs e)
+    {
+      Assert.ArgumentNotNull(e, "e");
+      if (e.Name == "lockeditems:refresh")
+      {
+        SheerResponse.Eval("Items.callback()");
+      }
+      else
+      {
+        base.ExecuteAjaxCommand(e);
+      }
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+      Assert.CanRunApplication("/sitecore/content/Applications/Content Editor/Ribbons/Chunks/Locks/My Items");
+      Assert.ArgumentNotNull(e, "e");
+      base.OnLoad(e);
+      if (!XamlControl.AjaxScriptManager.IsEvent)
+      {
+        Item[] elements = Client.ContentDatabase.SelectItems("search://*[@__lock='%\"" + Sitecore.Context.User.Name + "\"']") ?? new Item[0];
+        ComponentArtGridHandler<Item>.Manage(this.Items, new GridSource<Item>(elements), true);
+        this.Items.GroupingNotificationText = Translate.Text("To group your items by column, drag and drop the column here.");
+        this.Items.LocalizeGrid();
+      }
+    }
+  }
+
+}
