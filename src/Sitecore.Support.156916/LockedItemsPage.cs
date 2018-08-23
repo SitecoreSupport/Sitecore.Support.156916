e9359d65bda24c2ecaaf4cb9d0362e3d01273902
Calvin Williams <40271790+sc-CalvinWilliams@users.noreply.github.com>
2018-08-23 14:46:03 -0400

    Patch for version 8.2.4

---
+++ src/Sitecore.Support.156916/LockedItemsPage.cs
@@ -0,0 +1,122 @@
+﻿using ComponentArt.Web.UI;
+using Sitecore;
+using Sitecore.Controls;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Extensions;
+using Sitecore.Globalization;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web;
+using Sitecore.Web.UI.Grids;
+using Sitecore.Web.UI.Sheer;
+using Sitecore.Web.UI.XamlSharp.Ajax;
+using Sitecore.Web.UI.XamlSharp.Xaml;
+using System;
+using System.Linq;
+namespace Sitecore.Support.Shell.Applications.WebEdit.Dialogs.LockedItems
+{
+  /// <summary>
+  /// The locked items page.
+  /// </summary>
+  public class LockedItemsPage : ModalDialogPage
+  {
+    /// <summary>
+    /// The unlocked item IDs list session key
+    /// </summary>
+    protected const string UnlockedItemsIDsKey = "UnlockedItemIDs";
+
+    /// <summary>
+    /// The items.
+    /// </summary>
+    protected Grid Items;
+
+    /// <summary>
+    /// Gets or sets unlocked item IDs
+    /// </summary>
+    protected string[] UnlockedItemIds
+    {
+      get
+      {
+        return WebUtil.GetSessionValue("UnlockedItemIDs") as string[];
+      }
+      set
+      {
+        WebUtil.SetSessionValue("UnlockedItemIDs", value);
+      }
+    }
+
+    /// <summary>
+    /// Executes the ajax command.
+    /// </summary>
+    /// <param name="e">
+    /// The <see cref="T:Sitecore.Web.UI.XamlSharp.Ajax.AjaxCommandEventArgs" /> instance containing the event data.
+    /// </param>
+    protected override void ExecuteAjaxCommand(AjaxCommandEventArgs e)
+    {
+      Assert.ArgumentNotNull(e, "e");
+      if (e.Name == "lockeditems:refresh")
+      {
+        SheerResponse.Eval("Items.callback()");
+      }
+      else
+      {
+        base.ExecuteAjaxCommand(e);
+      }
+    }
+
+    /// <summary>
+    /// Raises the <see cref="E:System.Web.UI.Control.Load"></see> event.
+    /// </summary>
+    /// <param name="e">
+    /// The <see cref="T:System.EventArgs"></see> object that contains the event data.
+    /// </param>
+    protected override void OnLoad(EventArgs e)
+    {
+      Assert.CanRunApplication("/sitecore/content/Applications/Content Editor/Ribbons/Chunks/Locks/My Items");
+      Assert.ArgumentNotNull(e, "e");
+      base.OnLoad(e);
+      if (!XamlControl.AjaxScriptManager.IsEvent)
+      {
+        string[] unlockedItemIds = UnlockedItemIds;
+        WebUtil.RemoveSessionValue("UnlockedItemIDs");
+        Item[] array = Client.ContentDatabase.SelectItems("search://*[@__lock='%\"" + Sitecore.Context.User.Name + "\"']") ?? new Item[0];
+        if (unlockedItemIds != null)
+        {
+          array = ((unlockedItemIds.Length == 0) ? new Item[0] : (from x in array
+                                                                  where !unlockedItemIds.Contains(x.ID.ToString())
+                                                                  select x).ToArray());
+        }
+        ComponentArtGridHandler<Item>.Manage(Items, new GridSource<Item>(array), true);
+        Items.GroupingNotificationText = Translate.Text("To group your items by column, drag and drop the column here.");
+        Items.LocalizeGrid();
+      }
+    }
+
+    /// <summary>
+    /// Hanldes Unlock button click.
+    /// </summary>
+    protected void OnUnlock()
+    {
+      string[] selectedKeys = Items.SelectedKeys;
+      if (selectedKeys.Length > 0)
+      {
+        UnlockedItemIds = selectedKeys;
+      }
+      Command command = CommandManager.GetCommand("webedit:unlock");
+      CommandContext context = new CommandContext();
+      command.Execute(context);
+    }
+
+    /// <summary>
+    /// Hanldes UnlockAll button click.
+    /// </summary>
+    protected void OnUnlockAll()
+    {
+      UnlockedItemIds = new string[0];
+      Command command = CommandManager.GetCommand("webedit:unlockall");
+      CommandContext context = new CommandContext();
+      command.Execute(context);
+    }
+  }
+
+}
---
+++ src/Sitecore.Support.156916/UnlockAll.cs
@@ -0,0 +1,91 @@
+﻿using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.Jobs;
+using Sitecore.Shell.Applications.Dialogs.ProgressBoxes;
+using Sitecore.Shell.Applications.WebEdit.Commands;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
+using Sitecore.Web.UI.WebControls;
+using Sitecore.Web.UI.XamlSharp.Continuations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Sitecore.Support.Shell.Applications.WebEdit.Commands
+{
+  [Serializable]
+  public class UnlockAll : WebEditCommand, ISupportsContinuation
+  {
+    public override void Execute(CommandContext context)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      ContinuationManager.Current.Start(this, "Run");
+    }
+
+    protected static void Run(ClientPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      List<Item> list = new List<Item>();
+      Item[] array = Client.ContentDatabase.SelectItems("search://*[@__lock='%\"" + Context.User.Name + "\"']");
+      if (array == null || !array.Any())
+      {
+        SheerResponse.Alert("You have no locked items.");
+      }
+      else if (args.IsPostBack)
+      {
+        if (args.Result == "yes")
+        {
+          Item[] array2 = array;
+          foreach (Item item in array2)
+          {
+            Item[] versions = item.Versions.GetVersions(true);
+            foreach (Item item2 in versions)
+            {
+              if (string.Compare(item2.Locking.GetOwner(), Context.User.Name, StringComparison.InvariantCultureIgnoreCase) == 0)
+              {
+                list.Add(item2);
+              }
+            }
+          }
+          ProgressBox.Execute("UnlockAll", "Unlocking items", "Network/16x16/lock.png", UnlockAllItems, "lockeditems:refresh", Context.User, list);
+        }
+      }
+      else
+      {
+        if (array.Count() == 1)
+        {
+          SheerResponse.Confirm("Are you sure you want to unlock this item?");
+        }
+        else
+        {
+          SheerResponse.Confirm(Translate.Text("Are you sure you want to unlock these {0} items?", array.Count()));
+        }
+        args.WaitForPostBack();
+      }
+    }
+
+    private static void UnlockAllItems(params object[] parameters)
+    {
+      Assert.ArgumentNotNull(parameters, "parameters");
+      List<Item> list = parameters[0] as List<Item>;
+      if (list != null)
+      {
+        Job job = Context.Job;
+        if (job != null)
+        {
+          job.Status.Total = list.Count;
+        }
+        foreach (Item item in list)
+        {
+          job?.Status.Messages.Add(Translate.Text("Unlocking {0}", item.Paths.ContentPath));
+          item.Locking.Unlock();
+          if (job != null)
+          {
+            job.Status.Processed += 1L;
+          }
+        }
+      }
+    }
+  }
+}
